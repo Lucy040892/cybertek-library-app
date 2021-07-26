7af1985a44b3b7cd85dc98f4b9eec2a787338fa4
Lucy040892 <Komisarenkoliudmyla@gmail.com>
2021-07-26 13:48:42 -0500

    Library app

--- src/test/java/com/cybertek/library/pages/UsersPage.java
+++ src/test/java/com/cybertek/library/pages/UsersPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class UsersPage {
 
     public UsersPage() {
@@ -13,5 +15,10 @@ public class UsersPage {
 
     @FindBy(name = "tbl_users_length")
     public WebElement showRecordsDropdowns;
+
+    //@FindBy(xpath = "//th")
+    @FindBy(tagName = "th")
+    public List<WebElement> tableHeaders;
+
 
 }
